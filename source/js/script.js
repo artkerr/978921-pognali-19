fa3c0f73d25682570099e0b1f5c99707e5bdb43e
Denis Vladimirov <denis@artkerr.com>
2020-05-26 21:42:21 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -27,4 +27,38 @@ window.addEventListener("keydown", function (evt) {
     2. Проверить насколько прошел скролл
     2.1 Если скролл > 0 добавляем класс sticky, и заменяем картинки
     2.2 Если скролл = 0 убираем класс sticky, и заменяем картинки
+
+var headerElement = document.querySelector(".page-header__wrapper-sticky");
+console.log();
+changeImagePath(selector, state) {
+  document.querySelector("").srcset = state;
+  console.log();
+};
+
+document.addEventListener("scroll", function() {
+  var scrollY = window.scrollY;
+  console.log();
+
+  if (scrollY > 0) {
+    console.log('Вниз');
+    headerElement.classList.add('open');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+} else {
+    console.log('Норм');
+    headerElement.classList.remove("open");
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+    changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x');
+}
+});
+
+
 */
