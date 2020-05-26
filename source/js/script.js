317d81e1bb06b411e47501eca80d919cad9fb691
Denis Vladimirov <denis@artkerr.com>
2020-05-26 19:12:57 +0300

    Правки

--- source/js/script.js
+++ source/js/script.js
@@ -21,36 +21,10 @@ window.addEventListener("keydown", function (evt) {
   }
 });
 
+
 /*
     1. Отловить собитые скрола
     2. Проверить насколько прошел скролл
     2.1 Если скролл > 0 добавляем класс sticky, и заменяем картинки
     2.2 Если скролл = 0 убираем класс sticky, и заменяем картинки
 */
-
-headerElement = window.querySelector('.js-header');
-
-window.addEventListener('scroll', function(event) {
-    var scrollY = window.scrollY;
-
-    if (scrollY > 0) {
-        console.log('Вниз');
-        headerElement.addClass('open');
-        changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x')
-        changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x')
-        changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x')
-        changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x')
-        changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x')
-        changeImagePath(".js-header-logo-tablet-png", 'img/logo-desktop-blue@1x.webp, img/logo-desktop-white@2x.webp 2x')
-    } else {
-        console.log('Норм');
-    }
-});
-
-changeImagePath(selector, state) {
-    document.querySelector(selector).srcset = state;
-};
-
-document.querySelector(".js-header-logo-tablet-png").srcset = "img/logo-tablet-" + n + "@1x.png 1x, img/logo-tablet-" + n + "@2x.png 2x";
-
-
